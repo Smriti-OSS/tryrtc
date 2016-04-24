1a7f351d6b7bf18999a664becb544b10a483e06b
smriti-thakur <smritisamta@gmail.com>
2016-04-24 16:37:24 -0700

    set porttt

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var static = require('node-static');
 var http = require('http');
 var file = new(static.Server)();
+
+//var app = http.createServer(function (req, res) {
+//  file.serve(req, res);
+//}).listen(8080);
 var app = http.createServer(function (req, res) {
   file.serve(req, res);
-}).listen(8080);
-
+}).set('port',process.env.PORT||3000);
 var io = require('socket.io').listen(app);
 io.sockets.on('connection', function (socket){
 
