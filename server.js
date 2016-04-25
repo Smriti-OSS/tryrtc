2613a3603547b49decb4a39a1ed1de63b3d20a9e
smriti-thakur <smritisamta@gmail.com>
2016-04-24 23:07:50 -0700

    procees....port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var file = new(static.Server)();
 //}).listen(8080);
 var app = http.createServer(function (req, res) {
   file.serve(req, res);
-}).listen(3000);
+}).listen(process.env.PORT||5000);
 var io = require('socket.io').listen(app);
 io.sockets.on('connection', function (socket){
 
